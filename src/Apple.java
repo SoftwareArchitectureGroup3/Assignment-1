51c4ef860f71481856740e97269a2d283426d9d7
Soumil15 <94348625+Soumil15@users.noreply.github.com>
2022-10-06 16:52:38 -0400

    Update Apple.java

--- src/Apple.java
+++ src/Apple.java
@@ -1,4 +1,5 @@
 public interface Apple{
+    // This interface provides the setApplePrice() method that reads the price from File1.txt and stores the price for every variant of Apple
     void setApplePrice();
 
 }
