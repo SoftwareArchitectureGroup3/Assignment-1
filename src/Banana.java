fd7c2b1665eb194621bdee8b06f1166ed22bc826
angadsingh21 <94552675+angadsingh21@users.noreply.github.com>
2022-10-06 16:57:25 -0400

    Update Banana.java
    Bhai please accept karle na. Please yaar

--- src/Banana.java
+++ src/Banana.java
@@ -1,6 +1,5 @@
 public interface Banana {
-
+    // This interface provides the setBananaPrice() method that reads the price from File1.txt and stores the price for each Banana Variant
     void setBananaPrice();
-
 
 }
