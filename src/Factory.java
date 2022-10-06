09eea9b7363d472453ddb97caf4404e85d59b930
Soumil15 <94348625+Soumil15@users.noreply.github.com>
2022-10-06 16:58:40 -0400

    Update Factory.java

--- src/Factory.java
+++ src/Factory.java
@@ -1,7 +1,11 @@
 public class Factory implements GroceryProductFactory {
+    
+    //The Factory class implements the GroceryProductFactory and overrides the methods present in the interface
+    //The Factory class is responsible for creating the products i.e Apples and Banana
 
     @Override
     public Apple createApple(String type) {
+        //The object creation depends on the input that user passes to the method
         if (type.equals("1")) {
             return new Apple_1();
         }
